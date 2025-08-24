48fbc1b1838d8a4455a2d2dd903dcb908e644030
Muhammedzubair10 <kmohammedzubair404@gmail.com>
2025-08-24 16:55:04 +0530

    Update signup_page.py

--- pages/signup_page.py
+++ pages/signup_page.py
@@ -2,14 +2,12 @@ import streamlit as st
 from backend.auth import signup_user
 
 def signup_page():
-    st.title("📝 Sign Up Page")
-
-    username = st.text_input("Username")
+    st.title("📝 Sign Up")
     email = st.text_input("Email")
     password = st.text_input("Password", type="password")
 
     if st.button("Sign Up"):
-        if signup_user(username, email, password):
-            st.success("Signup successful! Please go to the Login page.")
+        if signup_user(email, password):
+            st.success("Signup successful! ✅ Now you can login.")
         else:
-            st.error("Signup failed. Email may already exist.")
+            st.error("User already exists ❌")
